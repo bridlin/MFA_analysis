9c40399f5e5c5a9fc5c72bfc3500b302becf1ebd
bridlin <bridlinbarckmann@gmail.com>
2023-07-19 13:27:10 +0200

    removed unness modules and added fastqc step

--- trim_and_map.sh
+++ trim_and_map.sh
@@ -11,18 +11,20 @@
 #SBATCH --mem 64GB
 
 
-module load cutadapt/4.0
+
 module load fastqc/0.11.9
 module load bowtie2/2.4.1
 module load samtools/1.13
 module load trim-galore/0.6.5
-module load deeptools/3.5.0
+
 
 source scripts/MFA_analysis/config.txt
 
 mkdir $output_dir
 for x in "${input_list[@]}"; do
 trim_galore --paired --output_dir $input_dir/ $input_dir/$x\_1.fastq.gz  $input_dir/$x\_2.fastq.gz && 
+fastqc $input_dir/$x\_1_val_1.fq.gz
+fastqc $input_dir/$x\_2_val_2.fq.gz
 bowtie2 --very-senitive --threads 6 -x $genome -1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   -S $output_dir/$x\_$prefix\.sam &&
 samtools view  -b -q 20 $output_dir/$x\_$prefix\.sam > $output_dir/$x\_$prefix\_filtered.bam &&
 samtools sort $output_dir/$x\_$prefix\_filtered.bam -o $output_dir/$x\_$prefix\_filtered_sorted.bam &&
