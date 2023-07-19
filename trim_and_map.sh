93950f113346cf42ca5e412bf93b3ec89ee70f5c
bridlin <bridlinbarckmann@gmail.com>
2023-07-19 11:57:49 +0200

    added config file

---
+++ trim_and_map.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-type END
+#SBATCH --mail-user b-barckmann@chu-montpellier.fr
+#
+#
+#SBATCH --partition fast
+#SBATCH --cpus-per-task 6
+#SBATCH --mem 64GB
+
+
+module load cutadapt/4.0
+module load fastqc/0.11.9
+module load bowtie2/2.4.1
+module load samtools/1.13
+module load trim-galore/0.6.5
+module load deeptools/3.5.0
+
+source MFA_analysis/config.txt
+
+mkdir $output_dir
+for x in "${input_list[@]}"; do
+trim_galore --paired --output_dir $input_dir/ $input_dir/$x\_1.fastq.gz  $input_dir/$x\_2.fastq.gz && 
+# Briggs 2018 used the -very-sensitive option of bowtie2
+bowtie2 --threads 6 -x $genome -1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   -S $output_dir/$x\_$prefix\.sam &&
+samtools view  -b -q 20 $output_dir/$x\_$prefix\.sam > $output_dir/$x\_$prefix\_filtered.bam &&
+samtools sort $output_dir/$x\_$prefix\_filtered.bam -o $output_dir/$x\_$prefix\_filtered_sorted.bam &&
+rm -f  $output_dir/$x\_$prefix\.sam &&
+rm -f  $output_dir/$x\_$prefix\_filtered.bam  ; done
