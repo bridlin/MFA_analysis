5da732134732cb9d96a8eaa51d7a54b397058ce8
bridlin <bridlinbarckmann@gmail.com>
2023-07-19 11:58:26 +0200

    added Bowtie2 very-sensitve option

--- trim_and_map.sh
+++ trim_and_map.sh
@@ -24,7 +24,7 @@ mkdir $output_dir
 for x in "${input_list[@]}"; do
 trim_galore --paired --output_dir $input_dir/ $input_dir/$x\_1.fastq.gz  $input_dir/$x\_2.fastq.gz && 
 # Briggs 2018 used the -very-sensitive option of bowtie2
-bowtie2 --threads 6 -x $genome -1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   -S $output_dir/$x\_$prefix\.sam &&
+bowtie2 --very-senitive --threads 6 -x $genome -1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   -S $output_dir/$x\_$prefix\.sam &&
 samtools view  -b -q 20 $output_dir/$x\_$prefix\.sam > $output_dir/$x\_$prefix\_filtered.bam &&
 samtools sort $output_dir/$x\_$prefix\_filtered.bam -o $output_dir/$x\_$prefix\_filtered_sorted.bam &&
 rm -f  $output_dir/$x\_$prefix\.sam &&
