7844c1bdd929408c7c7dc2612d9fa5af1d063451
bridlin <bridlinbarckmann@gmail.com>
2023-07-20 09:42:51 +0200

    command out alignment

--- trim_and_map.sh
+++ trim_and_map.sh
@@ -22,9 +22,9 @@ source scripts/MFA_analysis/config.txt
 
 mkdir $output_dir
 for x in "${input_list[@]}"; do
-trim_galore --paired --output_dir $input_dir/ $input_dir/$x\_1.fastq.gz  $input_dir/$x\_2.fastq.gz && 
-fastqc $input_dir/$x\_1_val_1.fq.gz
-fastqc $input_dir/$x\_2_val_2.fq.gz
+# trim_galore --paired --output_dir $input_dir/ $input_dir/$x\_1.fastq.gz  $input_dir/$x\_2.fastq.gz && 
+# fastqc $input_dir/$x\_1_val_1.fq.gz
+# fastqc $input_dir/$x\_2_val_2.fq.gz
 bowtie2 --very-sensitive-local --threads 6 -x $genome -1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   -S $output_dir/$x\_$prefix\.sam &&
 samtools view  -b -q 20 $output_dir/$x\_$prefix\.sam > $output_dir/$x\_$prefix\_filtered.bam &&
 samtools sort $output_dir/$x\_$prefix\_filtered.bam -o $output_dir/$x\_$prefix\_filtered_sorted.bam &&
