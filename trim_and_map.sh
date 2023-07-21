cbc4a5afb839e851a6a010af440d67946f5bee4f
bridlin <bridlinbarckmann@gmail.com>
2023-07-21 16:32:42 +0200

    added -k1 parameter in bowtie2

--- trim_and_map.sh
+++ trim_and_map.sh
@@ -25,7 +25,7 @@ for x in "${input_list[@]}"; do
 # trim_galore --paired --output_dir $input_dir/ $input_dir/$x\_1.fastq.gz  $input_dir/$x\_2.fastq.gz && 
 # fastqc $input_dir/$x\_1_val_1.fq.gz
 # fastqc $input_dir/$x\_2_val_2.fq.gz
-bowtie2 --very-sensitive-local --threads 6 -x $genome -1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   -S $output_dir/$x\_$prefix\.sam &&
+bowtie2 -k1 --very-sensitive-local --threads 6 -x $genome -1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   -S $output_dir/$x\_$prefix\.sam &&
 samtools view  -b -q 20 $output_dir/$x\_$prefix\.sam > $output_dir/$x\_$prefix\_filtered.bam &&
 samtools sort $output_dir/$x\_$prefix\_filtered.bam -o $output_dir/$x\_$prefix\_filtered_sorted.bam &&
 rm -f  $output_dir/$x\_$prefix\.sam &&
